140ec74ee413703dbe53aae64df2310254a3aaa0
JinnyHui <hui@iupui.edu>
2018-09-24 00:19:10 -0400

    eigen value

--- JH_PCA.py
+++ JH_PCA.py
@@ -3,17 +3,16 @@
 # CSCI573 DATAMINING
 # HOMEWORK 2 PCA
 
-import sys
 import pandas as pd
 import numpy as np
 
 # read data from file into dataframe
-# file_name = sys.argv[1]
+# file_name = raw_input('Please input the name of data file:')
 raw_data = pd.read_csv('iris.txt', header=None)
 raw_data_array = np.array(raw_data)
 row, column_label = raw_data_array.shape  # shape(19020, 10)
 column = column_label - 1
-data = raw_data_array[:, 0:column_label]  # slice the array to eliminate label column
+data = raw_data_array[:, 0: column]  # slice the array to eliminate label column
 data = np.float64(data)
 
 
@@ -22,7 +21,8 @@ mean_vec = np.mean(data, axis=0)
 # print(mean_vec)
 std_vec = np.std(data, axis=0)
 norm_data = (data - mean_vec)/std_vec
-#print(centered_data)
+# print('1. The normalized data is:')
+# print(norm_data)
 
 
 # 2nd step: Covariance Matrix
@@ -33,6 +33,29 @@ for i in range(row):
     cov_matrix = cov_matrix + np.outer(a, b)  # sum of outer product of each centered point
 cov_matrix = cov_matrix/row  # normalize by n
 # comparison = np.cov(norm_data.T)
-# print(cov_matrix - comparison)
+print('\n2. The covariance matrix is:')
+print(cov_matrix)
 
 # 3rd step: Dominate eigenvalue and eigenvector
+change = float('INF')
+vector_pre = np.zeros(column)
+vector_x = np.random.rand(column)  # initialize a random vector
+while change >= 0.000001:  # keep calculating the new vector until the change < threshold
+    vector_pre = vector_x
+    vector_unscaled_x = np.dot(cov_matrix, vector_x)
+    max_val = np.amax(np.absolute(vector_unscaled_x))  # get the max abs value
+    vector_x = vector_unscaled_x/max_val
+    change = np.linalg.norm(vector_x - vector_pre)
+norm = np.linalg.norm(vector_x)  # normalize the vector
+vector_norm_x = vector_x/norm
+max_x = np.amax(np.absolute(vector_unscaled_x))
+max_pre = np.amax(np.absolute(vector_pre))
+eigenvalue = max_x/max_pre  # calculate the max eigenvalue
+print('\n3. The maximum eigenvalue is:', str(eigenvalue))
+# print(np.linalg.eig(cov_matrix))
+
+# 4th step: Projection of the first two eigen vectors
+eigen_vec_1, eigen_vec_2 = np.linalg.eig(cov_matrix)[1][0:2]
+
+print(eigen_vec_1, eigen_vec_2)
+
