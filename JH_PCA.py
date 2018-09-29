2b3cbeea8cf44f97bf72a4a4900f0dcb3f4533cd
JinnyHui <hui@iupui.edu>
2018-09-29 19:28:11 -0400

    structured

--- JH_PCA.py
+++ JH_PCA.py
@@ -6,71 +6,124 @@
 import pandas as pd
 import numpy as np
 
-# read data from file into dataframe
+
+def file_reader(name):
+    """
+    read data from file into dataframe
+    :param name: file name input by user
+    :return: clean dataset
+    """
+    raw_data = pd.read_csv(name, header=None)
+    raw_data_array = np.array(raw_data)
+    row, column_label = raw_data_array.shape  # shape(19020, 10)
+    column = column_label - 1
+    data_clean = raw_data_array[:, 0: column]  # slice the array to eliminate label column
+    return np.float64(data_clean)
+
+
+def z_norm(dataset):
+    """
+    perform z-normalization
+    :param dataset: dataset
+    :return: normed data
+    """
+    mean_vec = np.mean(dataset, axis=0)
+    std_vec = np.std(dataset, axis=0)
+    data_norm = (data - mean_vec) / std_vec
+    return data_norm
+
+
+def cov_matrix(dataset):
+    """
+    compute the covariance matrix
+    :param dataset: cleaned dataset
+    :return: covariance matrix
+    """
+    row, column = dataset.shape
+    matrix = np.zeros((column, column))
+    for x in range(row):
+        a = dataset[x]  # each centered point
+        b = np.transpose(a)  # transpose of centered point
+        matrix = matrix + np.outer(a, b)  # sum of outer product of each centered point
+    matrix = matrix / row  # normalize by n
+    return matrix
+
+
+def eigen(dataset, e):
+    """
+    Compute dominant eigenvalue and eigenvector with power iteration
+    :param dataset: clean dataset
+    :param e: the threshold to converge
+    :return: tuple of eigenvalue and eigenvector
+    """
+    row, column = dataset.shape
+    change = float('INF')
+    vector_pre = np.zeros(column)
+    vector_x = np.random.rand(column)  # initialize a random vector
+    while change >= e:  # keep calculating the new vector until the change < threshold
+        vector_pre = vector_x
+        vector_unscaled_x = np.dot(cov_matrix, vector_x)
+        max_val = np.amax(np.absolute(vector_unscaled_x))  # get the max abs value
+        vector_x = vector_unscaled_x / max_val
+        change = np.linalg.norm(vector_x - vector_pre)
+    norm = np.linalg.norm(vector_x)
+    eigenvector = vector_x / norm  # normalize the vector
+    max_x = np.amax(np.absolute(vector_unscaled_x))
+    max_pre = np.amax(np.absolute(vector_pre))
+    eigenvalue = max_x / max_pre  # calculate the max eigenvalue
+    return eigenvalue, eigenvector
+
+###############################################################################################
+#                                 START OF THE PROGRAM                                        #
+###############################################################################################
+
+
 # file_name = raw_input('Please input the name of data file:')
-raw_data = pd.read_csv('iris.txt', header=None)
-raw_data_array = np.array(raw_data)
-row, column_label = raw_data_array.shape  # shape(19020, 10)
-column = column_label - 1
-data = raw_data_array[:, 0: column]  # slice the array to eliminate label column
-data = np.float64(data)
+data = file_reader('iris.txt')
 
+# step a: Z-Normalization
+norm_data = z_norm(data)
 
-# 1st step: Z-Normalization
-mean_vec = np.mean(data, axis=0)
-# print(mean_vec)
-std_vec = np.std(data, axis=0)
-norm_data = (data - mean_vec)/std_vec
-# print('1. The normalized data is:')
-# print(norm_data)
+# step b: Covariance Matrix
+cov_matrix = cov_matrix(norm_data)
+npSigma = np.cov(norm_data,  bias=True, rowvar=False)
 
-
-# 2nd step: Covariance Matrix
-cov_matrix = np.zeros((column, column))
-for i in range(row):
-    a = norm_data[i]  # each centered point
-    b = np.transpose(a)  # transpose of centered point
-    cov_matrix = cov_matrix + np.outer(a, b)  # sum of outer product of each centered point
-cov_matrix = cov_matrix/row  # normalize by n
-# comparison = np.cov(norm_data.T)
-print('\n2. The covariance matrix is:')
+print('\nb. The covariance matrix is:')
 print(cov_matrix)
+print('\n   comparison with np.cov:')
+print(npSigma)
 
-# 3rd step: Dominate eigenvalue and eigenvector
-change = float('INF')
-vector_pre = np.zeros(column)
-vector_x = np.random.rand(column)  # initialize a random vector
-while change >= 0.000001:  # keep calculating the new vector until the change < threshold
-    vector_pre = vector_x
-    vector_unscaled_x = np.dot(cov_matrix, vector_x)
-    max_val = np.amax(np.absolute(vector_unscaled_x))  # get the max abs value
-    vector_x = vector_unscaled_x/max_val
-    change = np.linalg.norm(vector_x - vector_pre)
-norm = np.linalg.norm(vector_x)  # normalize the vector
-vector_norm_x = vector_x/norm
-max_x = np.amax(np.absolute(vector_unscaled_x))
-max_pre = np.amax(np.absolute(vector_pre))
-eigenvalue = max_x/max_pre  # calculate the max eigenvalue
-print('\n3. The maximum eigenvalue is:', str(eigenvalue))
-# print(np.linalg.eig(cov_matrix))
 
-# 4th step: Projection on the first two eigen vectors
-eigen_vec = np.linalg.eig(cov_matrix)[1]
-eigen_vec_1 = eigen_vec[:, 0]
-eigen_vec_2 = eigen_vec[:, 1]
-projection = np.zeros((row, column))
-for i in range(row):  # compute the projected points
-    projection_1 = (np.dot(eigen_vec_1.T, norm_data[i]))*eigen_vec_1
-    projection_2 = (np.dot(eigen_vec_2.T, norm_data[i]))*eigen_vec_2
-    projection[i] = projection_1 + projection_2
+# step c: Dominant eigenvalue and eigenvector
+epsilon = 0.000001
+dominant_value, dominant_vector = eigen(norm_data, epsilon)
+print('\nc. My maximum eigenvalue is:', str(dominant_value))
+print('   My dominant eigenvector is:')
+print(dominant_vector)
+print('\nCompare with np linalg.eig method:')
+print(np.linalg.eig(cov_matrix))
+
+
+# step d: Projection on the first two eigen vectors
+eigen_vec = np.linalg.eig(cov_matrix)[1][:, 0:2]
+row_data, column_data = norm_data.shape
+projection = np.zeros((row_data, column_data))
+matrix_a = np.zeros((row_data, 2))
+for i in range(row_data):  # compute the projected points
+    matrix_a[i] = (np.dot(eigen_vec.T, norm_data[i]))
+for i in range(row_data):
+    projection[i] = np.dot(eigen_vec, matrix_a[i].T)
 variance = np.var(projection)
 print('\n4. The variance is:', str(variance))
 
-# 5th step: Covariance matrix in eigen-decomposition form
+# step e: Covariance matrix in eigen-decomposition form
 eigen_val = np.linalg.eig(cov_matrix)[0]
 similar_matrix = np.diag(eigen_val)
+eigen_matrix = eigen_vec = np.linalg.eig(cov_matrix)[1]
 cov_matrix_dec = np.dot(np.dot(eigen_vec, similar_matrix), eigen_vec.T)
 print('\n5. The Eigen-decomposition form of covariance matrix:')
-print(cov_matrix_dec)
+# print(cov_matrix_dec)
 
-# 6th step:
+# step f: PCA sub-routine
+def PCA(D, alpha):
+    return 
